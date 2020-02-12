a462b85cc7a22df09af8aa5596c6e08cc47e05ee
Wombola <54849261+Wombola@users.noreply.github.com>
2020-02-12 23:39:28 +0200

    Add files via upload
    I'm taking so long to make the simpliest stuff, i cry

---
+++ day3.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day1;
+import java.io.*;
+import java.util.*;
+
+
+/**
+ *
+ * @author Ace Dragon
+ */
+public class day3 {
+    FileWriter fileWriter;
+  {
+            try
+            {
+                    //what you doing tomorrow, export that to a txt file
+                    fileWriter = new FileWriter("C:list.txt");
+                    Scanner w = new Scanner(System.in);
+                    String r = w.nextLine();
+                fileWriter.write(r);
+                fileWriter.close();  
+            }catch(IOException e){}
+        }
+}
