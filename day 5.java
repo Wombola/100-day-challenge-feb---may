cd05c9de66a9057387a981b5cd5f6fc1f345b6ef
Wombola <54849261+Wombola@users.noreply.github.com>
2020-02-15 12:37:37 +0200

    Update day 5.java
    update

--- day 5.java
+++ day 5.java
@@ -1,4 +1,3 @@
-//super late, just got back home, time to code
 import java.io.*;
 import java.util.*;
 /**
@@ -10,34 +9,38 @@ public class day5 {
     public static void main(String[] args) {
                                    // TODO code application logic here
         System.out.println("what did you do today?");
-        Scanner s = new Scanner(System.in);
-        String t=s.nextLine();
+    Scanner s = new Scanner(System.in);
+        String today=s.nextLine();
         System.out.println("what did you forget to do");
-        String f=s.nextLine();
-        System.out.println("today, you did some: "+s+"you forgot to: "+f);
+        String forgot=s.nextLine();
+        
                                           // public static void main(String[] args) {
                                            //ask what you wanted to do today, did you do it(boolean)?
                                 //System.out.println("what did you want to do?");
                                    // Scanner u = new Scanner(System.in);
-    String i = s.nextLine();
-    System.out.println("did you do it?(true/false)");
     boolean did;
     String diddidnt = "";
-    did = s.nextBoolean();
+        System.out.println("did you do most of what you wanted to today?(true/false)");
+        String didu = s.nextLine();
+        did = s.nextBoolean();
                                                       // (y>n)? y:n;
     diddidnt = did ? "did good." : "need pull your socks up.";
-        System.out.println("today you wanted to "+i+" and in the end you "+diddidnt);
-            FileWriter fileWriter;
+        System.out.println("today you wanted to "+didu+" and in the end you "+diddidnt);
+        
+    FileWriter fileWriter;
   {
             try
             {
                                         //what you doing tomorrow, export that to a txt file
-                    fileWriter = new FileWriter("C:list.txt");
+    fileWriter = new FileWriter("C:list.txt");
                                           //Scanner w = new Scanner(System.in);
-                    String r = s.nextLine();
-                fileWriter.write(r);
-                fileWriter.close();  
-            }catch(IOException e){}
+        String tomorrow = s.nextLine();
+    fileWriter.write(tomorrow);
+    fileWriter.close();  
+            }catch(IOException e)
+            {
+            System.out.println("General I/O exception: " + e.getMessage());
+            }
         } try {
       File myObj = new File("C:list.txt");
       Scanner ss = new Scanner(myObj);
@@ -51,6 +54,6 @@ public class day5 {
       System.out.println("An error occurred.");
       e.printStackTrace();
     }
-  
+  System.out.println("today, you did some: "+today+"you forgot to: "+forgot);
 }
 }
